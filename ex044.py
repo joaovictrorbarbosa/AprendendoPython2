c1b33f249ecba96a3f3cbebaa9c422f250b03e27
Barbosa <joaovictorbarbosa2@gmail.com>
2024-12-02 20:10:04 -0300

    exercicio 44 -tabuada com for

---
+++ ex044.py
@@ -0,0 +1,5 @@
+tabuada = int(input('DIGITE QUAL NUMERO DESEJA DESCOBRIR SUA TABUADA: '))
+for cont in range (1,11):
+    print('{} x {:2} = {}'.format(tabuada , cont, tabuada*cont))
+print('FIM TABUADA')
+
