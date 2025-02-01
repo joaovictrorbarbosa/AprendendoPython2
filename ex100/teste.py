375f008c0d628455469d86d33757cd3a98b5b748
Barbosa <joaovictorbarbosa2@gmail.com>
2025-02-01 11:25:50 -0300

    exercicio 100 - transformando modulo em pacote de utilidades moeda

---
+++ ex100/teste.py
@@ -0,0 +1,4 @@
+from ex100.utilidadesjoao import moeda
+
+p = float(input('DIGITE O VALOR A SER ANALISANDO: R$ '))
+moeda.resumo(p, 20, 15)
---
+++ ex100/utilidadesjoao/__init__.py
@@ -0,0 +1,2 @@
+from ex100.utilidadesjoao import moeda
+from ex100.utilidadesjoao import dado
